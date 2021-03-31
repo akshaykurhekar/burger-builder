2309100200371350fc89590d98c3fada92ffa7fb
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-03-31 21:44:08 +0530

    form validation

--- src/component/UI/input/input.js
+++ src/component/UI/input/input.js
@@ -4,14 +4,40 @@ const input = (props) =>{
  
  let inputElement = null;
 
- switch ( props.inputtype ) {
+ const inputClasses = ["InputElement"];
+
+ if(props.invalid && props.shouldValidate && props.touched){
+     inputClasses.push("Invalid")
+ }
+
+ switch ( props.elementType ) {
      case 'input': 
-        inputElement = <input className="InputElement"  {...props} />         
+        inputElement = <input className={inputClasses.join(' ')}  
+          {...props.elementConfig }
+          value={props.value}
+          onChange={props.changed} />         
          break;
      case 'textarea': 
-     inputElement = <textarea className="InputElement" {...props} />                  
+     inputElement = <textarea className={inputClasses.join(' ') } 
+         {...props.elementConfig }
+         value={props.value} 
+         onChange={props.changed}/>                  
+         break;
+    case 'select': 
+     inputElement = (<select className={inputClasses.join(' ') } 
+                  value={props.value}
+                  onChange={props.changed} >
+                      {props.elementConfig.options.map( option =>(
+                          <option key={option.value} value={ option.value }>
+                          {option.displayValue}
+                          </option>
+                      ))}                     
+                  </select>);
+                                    
          break;      
-     default: inputElement = <input className="InputElement" {...props} />         
+     default: inputElement = <input className="InputElement" 
+         {...props.elementConfig }
+         value={props.value} />         
          break;
  }
     return(
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,55 +8,171 @@ import Input from "../../../component/UI/input/input";
 
 function ContactData (props) {
 
-    const [data, setData] = useState({
-        name:'',
-        email:'',
-        address: {
-            street:'',
-            postalCode: ''
+    const [orderForm, setOrderForm] = useState({
+        name:{
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Your Name'
+            },
+            value:'',
+            validation:{
+                 required: true   
+            },
+            valid: false,
+            touched: false
+        },
+        street:{
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Street'
+            },
+            value:'',
+            validation:{
+                required:true   
+           },
+           valid: false,
+           touched: false
+        },
+        zipCode: {
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Zip Code'
+            },
+            value:'',
+            validation: {
+                required:true,
+                minLength:'5',
+                maxLength:'5'   
+           },
+           valid: false,
+           touched: false
+        },
+        country:{
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Country'
+            },
+            value:'',
+            validation:{
+                required:true   
+           },
+           valid: false,
+           touched: false
+        },
+        email:{
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Your Email'
+            },
+            value:'',
+            validation:{
+                required:true   
+           },
+           valid: false,
+           touched: false
+        },
+        deliveryMethod:{
+            elementType:'select',
+            elementConfig:{
+                options:[
+                    {value:'fastest', displayValue:'Fastest'},
+                    {value:'cheapest', displayValue:'Cheapest'}
+
+                ]
+            },
+            value:''
         }
-    }, [ ]);
+        
+    });
 
     const [loading,setLoading] = useState(false);
 
-const orderHandler = (event) => {
-    event.preventDefault();
-    console.log(props.ingredients);
-    
-    setLoading(true);
-    
-    const order = {
-            ingredient: props.ingredients,
-            price: props.price,
-            customer:{
-                name:'Akshay Kurhekar',
-                address:{
-                    street:'Test street',
-                    zipCode: 444709,
-                    country: 'India'
-                },
-                email:'test@test.com'
-            },
-            deliveryMethod:'fastest'
+    const orderHandler = (event) => {
+        event.preventDefault();
+        setLoading(true);
+
+        const formData = {};
+
+        for(let id in orderForm){
+            formData[id] = orderForm[id].value;
         }
 
-        axios.post('/orders.json', order)
-        .then(response => { 
-            setLoading(false);
-            props.history.push('/');
-            })
-        .catch(error => {
-            setLoading(false);
+        const order = {
+                ingredient: props.ingredients,
+                price: props.price,
+                orderData: formData            
+            }
+
+            axios.post('/orders.json', order)
+            .then(response => { 
+                setLoading(false);
+                props.history.push('/');
+                })
+            .catch(error => {
+                setLoading(false);
+            });
+    }
+   const inputChangeHandler = (event, id) => {
+        const updatedOrderForm = { ...orderForm };
+        const updatedFormElement = { ...updatedOrderForm[id] }
+        
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation );
+        updatedFormElement.touched = true;
+        // console.log(updatedFormElement);
+        updatedOrderForm[id] = updatedFormElement;
+        
+        setOrderForm(updatedOrderForm);
+    }
+   
+    const checkValidity = (value, rules) => {
+        
+        let isValid = true;
+        
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid; 
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid ;
+        }
+        
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    const formElement = [];
+
+    for(let key in orderForm){
+        formElement.push({
+            id:key,
+            config: orderForm[key]
         });
-}
+    }
 
     let form = (
-        <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-                <Button btnType="Success" clicked={ orderHandler } >ORDER</Button>
+        <form onSubmit={orderHandler} >
+                
+                {formElement.map(formElement => (
+                    <Input 
+                           key={formElement.id}     
+                           elementType={formElement.config.elementType } 
+                           elementConfig={formElement.config.elementConfig } 
+                           value={formElement.config.value } 
+                           invalid = { !formElement.config.valid}
+                           shouldValidate = {formElement.config.validation }
+                           touched = {formElement.config.touched }
+                           changed={ (event) => inputChangeHandler(event,formElement.id) } />
+                ) )}
+                <Button btnType="Success">ORDER</Button>
             </form>
     );
 
